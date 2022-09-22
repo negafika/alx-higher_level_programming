dcfb651c5cf08da19ff28f00089569af06c9e5df
negafika <negafika17@gmail.com>
2022-09-22 14:36:41 +0300

    jahhaa

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    result = sum(list(map(int, argv)))
-    print(result)
+    num_args = len(argv)
+    total = 0
+    for i in range(1, num_args):
+        total += int(argv[i])
+    print("{:d}".format(total))
