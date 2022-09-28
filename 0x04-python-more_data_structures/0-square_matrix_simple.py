2e426e179790e1633b61a438711533ed21bf424d
negafika <negafika17@gmail.com>
2022-09-28 16:55:45 +0300

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,7 @@
 def square_matrix_simple(matrix=[]):
     new_matr = []
     for iterable in matrix:
-        new_matr.append(map(square,iterable))
+        new_matr.append(list(map(square,iterable)))
     return new_matr
-
-
 def square(n):
     return n*n
