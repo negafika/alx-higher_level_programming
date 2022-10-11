f696e273931414fdc6ca4cf586589e46a3d8e1e0
negafika <negafik17@gmail.com>
2022-10-11 10:05:03 -0700

    test

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except BaseException:
+        except:
             print(end="")
     print("")
     return count
