246bf9cf55c6da4a8deb16eb816bafbf10614080
negafika <negafik17@gmail.com>
2022-10-12 00:53:42 -0700

    Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        c = a/b
+    except ZeroDivisionError:
+        continue
+    finally:
+        print("Inside result: {}".format(c))
